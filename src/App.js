f488890bd5b647c349b6d44e77e56fd251204d0e
Gabriel Souza <gabrielpaiva423@gmail.com>
2022-06-18 20:41:13 -0300

    Finishing main's second part

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 // components
 import Header from './components/Header';
+import MainSpoonPart from './components/MainSpoonPart';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <Header/>
+        <MainSpoonPart/>
       </div>
     )
   }
---
+++ src/components/MainSpoonPart.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import * as S from "./styles/MainSpoonPartStyles"
+
+// image
+import spoonImage from "../assets/spoonImage.png"
+
+export default function MainSpoonPart() {
+    return (
+        <S.Div>
+            <S.SpoonImage src={spoonImage}/>
+            <S.AboutSection>
+                <S.AboutDiv>
+                    <S.TitleBox>
+                        <h2 style={{fontSize: '40px'}}>About</h2>
+                        <S.Line></S.Line>
+                    </S.TitleBox>
+                    <S.AboutText>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt, tortor nec rhoncus dictum, lorem massa tempus sem, eu tincidunt libero velit sit amet velit. Nunc in euismod urna. Duis dapibus, elit eu eleifend tincidunt, nulla ipsum consectetur lorem, quis tempor lorem justo quis nisi. Nam interdum, nisi nec mollis sagittis, enim risus euismod nisi, quis rutrum quam augue id mauris. Pellentesque mattis hendrerit semper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut vestibulum nisl ante, et ultricies sapien facilisis
+                    </S.AboutText>
+                </S.AboutDiv>
+            </S.AboutSection>            
+        </S.Div>
+    );
+}
