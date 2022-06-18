3b536188c02187806f189dabbff9707dbef836b0
Gabriel Souza <gabrielpaiva423@gmail.com>
2022-06-17 21:36:49 -0300

    Header done

---
+++ src/components/styles/HeaderStyles.js
@@ -0,0 +1,39 @@
+import styled from "styled-components"
+
+export const Div = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 100%;
+height: 1303px;
+`
+export const HeaderBar = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+width: 95%;
+`
+export const HeaderNav = styled.nav`
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+width: 45%;
+height: fit-content;
+`
+export const HeaderNavDiv = styled.ul`
+display: flex;
+justify-content: center;
+align-items: center;
+padding: 0;
+width: 32%;
+height: 81px;
+&:hover {
+    border: solid 3px;
+}
+`
+export const HeaderNavP = styled.p`
+font-size: 24px;
+`
+export const HeaderTitle = styled.div`
+margin-top: 26em;
+`
