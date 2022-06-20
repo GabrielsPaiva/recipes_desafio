262f1701a472a068c6fc30c1e3163852c370d029
Gabriel Souza <gabrielpaiva423@gmail.com>
2022-06-19 21:15:40 -0300

    feat: added footer aquives & main's latest recipes styles file

---
+++ src/components/styles/MainLatestRecipesStyles.js
@@ -0,0 +1,144 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+background-color: #F2F4F1;
+display: flex;
+flex-direction: column;
+align-items: center;
+width: 100%;
+height: 1503px;
+`
+export const DivTitle = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+height: 25vh;
+padding: 15vh;
+`
+export const MainTitle = styled.h3`
+font-family: 'Roboto', sans-serif;
+font-weight: 900;
+color: #373737;
+font-size: 40px;
+`
+export const Line = styled.div`
+width: 104px;
+height: 1px;
+margin: 15px;
+border: solid #373737;
+`
+export const MenuOptions = styled.div`
+/* border: solid green; */
+display: flex;
+justify-content: space-evenly;
+width: 1263px;
+height: 500px;
+background-color: #F2F4F1;
+`
+export const RedVelvet = styled.div`
+width: 170px;
+display: flex;
+flex-direction: column;
+margin: 22px;
+&:hover{
+    transform:scale(1.1);
+    cursor: pointer;
+}
+`
+export const CakeImg = styled.img`
+width: 300px;
+`
+export const MargheritaPizza = styled.div`
+width: 170px;
+display: flex;
+flex-direction: column;
+margin: 22px;
+&:hover{
+    transform:scale(1.1);
+    cursor: pointer;
+}
+`
+export const PizzaImg = styled.img`
+width: 300px;
+`
+export const PeanutSmoothie = styled.div`
+display: flex;
+flex-direction: column;
+margin: 22px;
+&:hover{
+    transform:scale(1.1);
+    cursor: pointer;
+}
+`
+export const SmoothieImg = styled.img`
+width: 300px;
+`
+export const FoodNameDiv = styled.div`
+width: 300px;
+height: 200px;
+background-color: #fff;
+display: flex;
+justify-content: center;
+box-shadow: 0px 7px 6px #00000029;
+`
+export const FirstDivLine = styled.div`
+width: 35px;
+height: 1px;
+margin: 90px;
+border: solid #373737;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+export const FoodNameTitle = styled.h3`
+font-size: 20px;
+font-weight: 300;
+width: 140px;
+margin: 20px;
+`
+export const FoodNameDivTwo = styled.div`
+width: 300px;
+height: 200px;
+background-color: #fff;
+display: flex;
+justify-content: center;
+box-shadow: 0px 7px 6px #00000029;
+`
+export const SecondDivLine = styled.div`
+width: 35px;
+height: 1px;
+margin: 90px;
+border: solid #373737;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+export const FoodNameTitleTwo = styled.h3`
+font-size: 20px;
+font-weight: 300;
+width: 140px;
+margin: 20px;
+`
+export const FoodNameDivThree = styled.div`
+width: 300px;
+height: 200px;
+background-color: #fff;
+display: flex;
+justify-content: center;
+box-shadow: 0px 7px 6px #00000029;
+`
+export const ThirdDivLine = styled.div`
+width: 35px;
+height: 1px;
+margin: 90px;
+border: solid #373737;
+display: flex;
+flex-direction: column;
+align-items: center;
+`
+export const FoodNameTitleThree = styled.h3`
+font-size: 20px;
+font-weight: 300;
+width: 140px;
+margin: 20px;
+`
